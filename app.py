6d459efad9888c536b15c39bee1411093206646e
Pablo Vinicius <viniciusdesousaa20@gmail.com>
2026-07-27 16:31:13 -0300

    Ajustando as funções

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-print("Sabor express\n")
+import subprocess
+import os
+from turtle import home
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair\n")
+def exibir_nome_do_progama():
+    print("Sabor express\n")
 
-opcao_escolhida = input("Escolha uma opção: ")
-print("Você escolheu a opção", opcao_escolhida)
+def exibir_menu():
+    print("Menu de opções:\n")  
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurantes")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
+
+def finalizar_programa():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Programa finalizado.\n")
+
+def escolher_opcao():
+    print("Escolha uma opção do menu acima:\n")
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    print(f"Você escolheu a opção {opcao_escolhida}\n")
+
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finalizar_programa()
+
+def main():
+    exibir_nome_do_progama()
+    exibir_menu()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
