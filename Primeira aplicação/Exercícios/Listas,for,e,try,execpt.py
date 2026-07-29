b45648e33da6a5d6c97f1afe6a26f30e3c0b4c76
Pablo Vinicius <viniciusdesousaa20@gmail.com>
2026-07-29 16:01:01 -0300

    Reorganizando

---
+++ Primeira aplicação/Exercícios/Listas,for,e,try,execpt.py
@@ -0,0 +1,14 @@
+#1. Crie uma lista para cada informação a seguir:
+
+# Lista de números de 1 a 10
+lista_de_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in lista_de_numeros:
+    print(i)
+
+# lista com quatro nomes;
+lista_de_nomes = ["Pablo", "Kethully", "Aline", "João Miguel"]
+
+# lista com ano que você nasceu e o ano atual.
+lista_de_anos = [2000, 2026]
+
+#2. Crie uma lista e ultilize um loop for para percorrer todos os elementos da lista.
